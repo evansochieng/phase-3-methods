0c67f5eb8e94a4de210b49433cdb65174ad2d4fb
Evans Ochieng <evansochieng121@gmail.com>
2022-10-17 14:55:59 +0300

    Rewrite JS functions as Ruby methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+
+# greet_programmer method
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# greet method
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# greet_with_default method
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# add method
+def add(num1, num2)
+    num1 + num2
+end
+
+# halve method
+def halve(num)
+    if num.class != Integer
+        return nil
+    end
+
+    num / 2
+    
+end
